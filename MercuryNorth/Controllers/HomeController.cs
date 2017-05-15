5bff1d2d9408736d6e85b1bd3ede7994a6dcad14
ckriutz <casey.kriutzfield@live.com>
2017-05-15 20:21:57 +0000

    Initial

---
+++ MercuryNorth/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MercuryNorth.Controllers
+{
+    public class HomeController : Controller
+    {
+        Random _random = new Random();
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Machine = Environment.MachineName;
+            ViewBag.FoundingYear = _random.Next(1940, 1991);
+            return View();
+        }
+    }
+}
