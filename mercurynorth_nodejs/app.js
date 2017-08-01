747b2841fe5283349aafa3d2487d3ab4b81c33e9
ckriutz <casey.kriutzfield@live.com>
2017-08-01 20:10:11 +0000

    Added ejs for templating.

--- mercurynorth_nodejs/app.js
+++ mercurynorth_nodejs/app.js
@@ -1,15 +1,26 @@
 var express = require('express');
+var os = require('os');
 
 var app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
 
 var port = 5000;
 
 app.use(express.static('public'));
-app.use(express.static('src/views'));
 
-app.get('/', function(request, response) {
-  response.send('Mercury North');
-});
+app.get('/', (req, res) => {
+  var year = random(1950, 1998);
+  // render `home.ejs` with the list of posts
+  res.render('index', { year: year, machine: os.hostname() })
+})
+
 app.listen(port, function(err) {
    console.log('running server on port ' + port);
+   console.log('running on ' + os.hostname());
 });
+
+function random (low, high) {
+    return Math.floor(Math.random() * (high - low) + low);
+}
